fb4f82795bb3e87bf46c9bc2beec5d3c93cc185e
Shengjiang Quan <qsj287068067@126.com>
2024-06-13 18:10:45 +0800

    offline_build: add instructions for the first time build
    Signed-off-by: Shengjiang Quan <qsj287068067@126.com>

--- offline_build.sh
+++ offline_build.sh
@@ -6,6 +6,15 @@
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+# Run the following line manually if you are building
+# the firmware on your build system for the first time.
+# Check the target module, selected elements, etc. to make
+# sure the current config is what you want.
+# After that, save and review the .config file and make sure
+# the selected elements are `=y`.
+
+#make menuconfig
+
 make defconfig
 make download -j$(nproc)
 make -j$(nproc) || make -j1 V=s
